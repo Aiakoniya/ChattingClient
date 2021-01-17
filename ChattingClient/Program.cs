9332bc23daf63af0117212d218a514217b4fe3c2
aiger <aigerima.nurlanova@gmail.com>
2021-01-17 22:19:13 +0600

    Добавьте файлы проекта.

---
+++ ChattingClient/Program.cs
@@ -0,0 +1,34 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using GRPChatProject;
+using System;
+using System.Threading.Tasks;
+
+namespace ChattingClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.Write("Please enter your name: ");
+            var username = Console.ReadLine();
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new ChatService.ChatServiceClient(channel);
+
+            using (var chat = client.Join())
+            {
+                _ = Task.Run(async () =>
+                {
+                    while (await chat.ResponseStream.MoveNext())
+                    {
+                        var response = chat.ResponseStream.Current;
+                        Console.WriteLine($"{response.User} : {response.Text}");
+                    }
+                }
+                );
+            }
+
+        }
+    }
+}
